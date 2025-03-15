02d4ff0cabd516e2d0972b089294a3b70e3877c2
AlineNRS <alinenasrodrigues@gmail.com>
2025-03-15 19:27:47 -0300

    insere a nova versão do projeto integrado com o mongodb

---
+++ mulherModel.js
@@ -0,0 +1,22 @@
+const mogoose = require('mongoose')
+
+const MulherSchema = new mogoose.Schema({
+    nome: {
+        type: String,
+        required: true
+    },
+    imagem: {
+        type: String,
+        required: true
+    },
+    citacao: {
+        type: String,
+        required: true
+    },
+    minibio: {
+        type: String,
+        required: true
+    }
+})
+
+module.exports = mogoose.model('diva', MulherSchema)
--- mulheres.js
+++ mulheres.js
@@ -1,30 +1,90 @@
-const express = require("express")
-const router = express.Router()
+const express = require("express")//estou iniciando o express
+const router = express.Router()// estou configurando a primeira parte da rota
+const cors = require('cors')//estou trazendo o pacote cors que permite consumir essa api no front end.
+const conectaBancoDeDados = require('./bancoDeDados')//estou ligando ao arquivo bancoDeDados
+conectaBancoDeDados()//eestou chamando a função que conecta o banco de dados
 
-const app = express()
-const porta = 3333
+const mulher = require('./mulherModel')
 
-const mulheres = [
-    {
-        nome:'Aline Rodrigues',
-        imagem:'https://media.licdn.com/dms/image/v2/C4D22AQG5eUHFgsYRgA/feedshare-shrink_2048_1536/feedshare-shrink_2048_1536/0/1661433549707?e=1743033600&v=beta&t=GA8X0_SWlQqyKr4i-XquTFHZOIyR1i9t7VezwJV-6z0',
-        minibio: 'Sou uma apaixonada pela qualidade de software (QA) e estou comprometida em garantir que nossos produtos atendam aos mais altos padrões. Minha abordagem voltada para resultados e minha capacidade de colaboração eficazmente com equipes multifuncionais me permite contribuir para o sucesso do projeto e a satisfação do cliente.'
-    },
-    {
-        nome:'Maria Julia Rodrigues',
-        imagem:'Dançarina',
-        minibio:'Estudante dançarina'
+const app = express()// estou iniciando o app
+app.use(express.json())
+app.use(cors())
 
+const porta = 3333//estou criando a porta
+
+//GET
+async function mostraMulheres(request, response) {
+    try {
+        const mulheresVindasDoBancoDeDados = await mulher.find()
+
+       response.json(mulheresVindasDoBancoDeDados)
+    } catch (error) {
+        console.log(erro)
     }
-]
+   }
 
-function mostraMulheres(request, response) {
-    response.json(mulheres)
+//POST
+async function criaMulher(request,response){
+  const novaMulher = new mulher({    
+    nome: request.body.nome,
+    imagem: request.body.imagem,
+    minibio: request.body.minibio,
+    citacao: request.body.citacao
+  })
+
+  try {
+    const mulherCriada = await novaMulher.save()
+    response.status(201).json(mulherCriada)
+  } catch (error) {
+    console.log(erro)
+  }
 }
 
+//PATCH
+async function corrigeMulher(request,response){
+    try {
+        const mulherEncontrada = await mulher.findById(request.params.id)
+
+        if (request.body.nome) {
+            mulherEncontrada.nome = request.body.nome
+        }
+        if (request.body.minibio){
+            mulherEncontrada.minibio = request.body.minibio
+        }
+        if(request.body.nome) {
+            mulherEncontrada.imagem = request.body.imagem
+        }
+        if (request.body.citacao) {
+            mulherEncontrada = request.body.citacao
+        }
+
+        const mulherAtualizadaNoBancoDeDados = await mulherEncontrada.save()
+
+        response.json(mulherAtualizadaNoBancoDeDados)
+    } catch (error) {
+        console.log(erro)
+    }       
+  }
+
+//DELETE
+async function deletaMulher(request,response) {
+   try {
+   await mulher.findByIdAndDelete(request.params.id)
+   response.json({messagem:'Mulher deletada com sucesso!'}) 
+   } catch (error) {
+    console.log(erro)
+   }
+   }
+
+  app.use(router.get('/mulheres', mostraMulheres))//configurei rota GET /mulheres
+  app.use(router.post('/mulheres', criaMulher)) //configurei rota POST /mulheres
+  app.use(router.patch('/mulheres/:id', corrigeMulher)) //configurei a rota PATCH /mulheres/:id
+  app.use(router.delete('/mulheres/:id', deletaMulher)) //configurei a rota DELETE //mulheres/:id
+
+  //porta
 function mostraPorta() {
     console.log("Servidor criado e rodando na porta ", porta)
 }
 
-app.use(router.get('/mulheres', mostraMulheres))
-app.listen(porta,mostraPorta)
+
+app.listen(porta,mostraPorta)//servidor ouvindo a porta
